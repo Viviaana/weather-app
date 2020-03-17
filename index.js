94a54de99cce2dc2deb5a0992fcf8aad6caf7086
Viviaana <kjingham91@gmail.com>
2020-03-17 09:57:55 +0000

    added link to the form

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const hbs = require('express-handlebars');
 const path = require('path');
 const express = require('express');
+const bodyParser = require("body-parser")
 
 const app = express();
 
@@ -10,6 +11,8 @@ const getWeather = require('./lib/getWeather')
 
 //USE PATH TO JOIN THE 2 PATHS - THESE WILL BE STATIC
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 //SET THE VIEW ENGINE TO HANDLEBARS
@@ -21,12 +24,19 @@ app.set('view engine', '.hbs');
 
 
 //RENDER THE INDEX.HBS PAGE
-app.get('/', async(req, res) =>{
-    let data = await getWeather();
+app.get('/', (req, res) =>{
+    res.render("index");
+});
+
+app.post("/", async(req, res) =>{
+    let city = req.body.city
+    let data = await getWeather(city);
+
     let temp = data.main.temp
     let country = data.sys.country
     let desc = data.weather[0].description
     let name = data.name
+
     res.render('index', {data:{temp, country, desc, name}});
 });
 
